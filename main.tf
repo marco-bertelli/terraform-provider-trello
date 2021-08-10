8652d995fcae17865ffc72a04203435c2d5feecb
Marco <marcobert37@gmail.com>
2021-08-10 16:53:44 +0200

    feat(project):code refactoring for project

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    custom = {
+    trello = {
       source = "hashicorp.com/edu/customprovider"
       version = "0.2"
     }
@@ -8,7 +8,7 @@ terraform {
 }
 
 
-resource "custom_server" "my-server-name" {
+resource "trello_board" "my-server-name" {
 	key = "e49bb022fb242733d8be9cffdcad6a96"
   token = "858bef8d6ed9e8bbc65e215b324a77be37e8c781410b1d76a4cc2333a6417005"
   workspace_name = "custom"
