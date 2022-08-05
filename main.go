21c2afd34b2ba630359a202bb592c562a4c342c4
Marco <marcobert37@gmail.com>
2022-08-05 17:15:54 +0200

    try fix

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
+)
+
 func main() {
         plugin.Serve(&plugin.ServeOpts{
                 ProviderFunc: func() terraform.ResourceProvider {
